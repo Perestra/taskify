fa0314bef13830fc046f6712b61da82c5cecc778
Perestra <danilo.ps.1996@gmail.com>
2024-09-13 08:11:28 -0300

    Created the Date component

---
+++ src/components/Date/Date.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './Date.module.scss'
+
+import { weekDay, month, day, year } from '../../helpers/getCurrentDate';
+
+export const Date = () => {
+  return (
+    <div className={ style.current }>
+      <h1 className={ style.current__weekDay }>{ weekDay }</h1>
+      <h3 className={ style.current__date }>{ `${day} ${month}, ${year}` }</h3>
+    </div>
+  )
+}
