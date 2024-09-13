b3248d036aa36fa52dc677e8e999e2f63ffbde08
Perestra <danilo.ps.1996@gmail.com>
2024-09-13 19:38:06 -0300

    Added the TaskList component in the Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import { Form } from '../../components/Form/Form'
+import { TaskList } from '../../components/TaskList/TaskList'
 import style from './Home.module.scss'
 
 export const Home = () => {
   return (
     <main className={ style.main }>
       <Form />
+      <TaskList />
     </main>
   )
 }
