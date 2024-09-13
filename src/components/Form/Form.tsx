be8f0582927ac5b7b9d90a4f9c7a53106925ea64
Perestra <danilo.ps.1996@gmail.com>
2024-09-13 10:04:47 -0300

    Created the Form component and removed from Home page

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,44 @@
+import { FormEvent, useState } from 'react'
+import style from './Form.module.scss'
+
+import { FiCheckSquare } from "react-icons/fi";
+import { Button } from '../../components/Button/Button';
+
+export const Form = () => {
+
+    const [task, setTask] = useState('')
+
+    const onSubmitHandler = (event: FormEvent) => {
+        event.preventDefault()
+        setTask('')
+    }
+
+  return (
+    <section className={ style.section }>
+        <div className={ style.section__container }>
+            <Button 
+                btnClass={ style.section__iconDiv }
+                iconClass={ style.section__icon }
+                type='button' 
+                icon={FiCheckSquare}
+            />
+            <form className={ style.section__form } onSubmit={onSubmitHandler}>
+                <input className={ style.section__input } 
+                    type="text"
+                    value={task}
+                    placeholder='Adicione uma tarefa...'
+                    onChange={event => setTask(event.target.value)}
+                />
+            </form>
+        </div>
+        <div className={ style.section__infos }>
+            <Button 
+                btnClass={ style.section__clean }
+                text='Limpar completas'
+                type='button' 
+            />
+            <span className={ style.section__remaining }>2 Tarefas restantes</span>
+        </div>
+      </section>
+  )
+}
