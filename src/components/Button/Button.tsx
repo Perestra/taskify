1eaeebe938488892171f9d3d5c014263f4d640ef
Perestra <danilo.ps.1996@gmail.com>
2024-09-13 08:09:33 -0300

    Deleted the button style file and add new props in the component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 
 type Props = {
-    text: string;
-    icon: React.ElementType;
-    onClick: (event: React.MouseEventHandler<HTMLButtonElement>) => void;
+  btnClass: string;
+  text?: string;
+  icon?: React.ElementType;
+  type: 'button' | 'submit' | 'reset';
+  iconClass: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const Button = ({ text, icon: Icon, onClick }: Props) => {
+export const Button = ({ btnClass, text, icon: Icon, type, iconClass, onClick }: Props) => {
   return (
     <button 
-        aria-label={ text }
-        type='button'
-        onClick={ () => onClick }
+      className={ btnClass }
+      aria-label={ text }
+      type={ type }
+      onClick={ () => onClick }
     >
-        <Icon />
-        <span>{ text }</span>
+      {Icon && <Icon className={ iconClass } />}
+      <span>{ text }</span>
     </button>
   )
 }
