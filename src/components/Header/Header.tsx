3a7207de04858e7555fbd7ac0ae6f864bc17b28c
Perestra <danilo.ps.1996@gmail.com>
2024-09-13 08:13:16 -0300

    Created the Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import style from './Header.module.scss'
+
+import links from '../../json/HeaderLinks.json'
+import { NavList } from '../NavList/NavList';
+
+type Link = {
+    id: number;
+    name: string;  
+    url: string;
+}
+
+export const NavHeader = () => {
+    return (
+        <header className={ style.header }>
+            <nav className={ style.header__nav }>
+                <ul className={ style.header__ul }>
+                    {links.map((nav: Link) => (
+                    <NavList key={ nav.id } text={ nav.name } to={ nav.url } />
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+  };
