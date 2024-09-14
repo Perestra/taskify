59ba2b4da27bd5d6251619bc7f9c04b564386f61
Perestra <danilo.ps.1996@gmail.com>
2024-09-14 14:52:09 -0300

    Created a RouterHeader component to use in Router structure

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-
-import { NavHeader } from "./components/Header/Header"
+import { TaskProvider } from "./contexts/TaskContext"
+import { RouterHeader } from "./RouterHeader/RouterHeader"
 import { Home } from "./pages/Home/Home"
-import { Date } from "./components/Date/Date"
 
 const Routers = () => {
   return (
     <BrowserRouter>
-      <NavHeader />
-      <Date />
-      <Routes>
-        <Route index element={<Home />} />
-      </Routes>
+      <RouterHeader/>
+      <TaskProvider>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </TaskProvider>
     </BrowserRouter>
   )
 }
