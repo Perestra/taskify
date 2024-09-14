776a555250c42a26cbcac4fcf285ba7d6cecd146
Perestra <danilo.ps.1996@gmail.com>
2024-09-14 14:50:47 -0300

    Added the TaskAction component in the Task

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
 import style from './Task.module.scss'
 
 import { CheckBox } from '../CheckBox/CheckBox'
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { TaskAction } from '../TaskAction/TaskAction';
+import { useState } from 'react';
 
 export const Task = () => {
+
+  const [dialogActive, setDialogActive] = useState(false)
+
   return (
     <div className={ style.task }>
-        <CheckBox />
-        <span className={ style.task__name }>Comer arroz e carne moidaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</span>
-        <HiOutlineDotsHorizontal className={ style.task__icon }/>
+      <CheckBox />
+      <span className={ style.task__name }>Comer arroz e carne moidaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</span>
+      <div className={ style.task__action }>
+        <HiOutlineDotsHorizontal className={ style.task__icon } onClick={() => setDialogActive(!dialogActive)}/>
+        <TaskAction open={dialogActive} />
+      </div>
     </div>
   )
 }
