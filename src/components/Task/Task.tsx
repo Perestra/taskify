2462e754a4d7cbc6513ef91b2447e34552feb155
Perestra <danilo.ps.1996@gmail.com>
2024-09-18 21:40:05 -0300

    Changed from span to input to used the contentEditable attribute

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -2,27 +2,49 @@ import style from './Task.module.scss'
 
 import { CheckBox } from '../CheckBox/CheckBox'
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { LuCheck } from "react-icons/lu";
 import { TaskAction } from '../TaskAction/TaskAction';
 import { useState } from 'react';
+import { useTaskContext } from '../../hooks/useTaskContext';
 
 type Props = {
-  text: string;
   id: string;
+  text: string;
   isDone: boolean;
 }
 
-export const Task = ({ text, id, isDone }: Props) => {
+export const Task = ({ id, text, isDone }: Props) => {
 
   const [dialogActive, setDialogActive] = useState(false)
+  const [isEditable, setIsEditable] = useState(false)
+  const [newTask, setNewTask] = useState('')
+  const { editTask } = useTaskContext()
+  
+  const editNameTask = (id: string, newName: string) => {
+    editTask(id, newName)
+    setIsEditable(false)
+  }
+
+  const isNameChanged = (text: string, newTask: string) => {
+    if(isEditable && newTask.trim().length > 0 && newTask !==text) return newTask
+    return text 
+  }
 
   return (
     <li className={ style.task }>
-      <div className={ style.task__container }>
+      <div className={ `${style.task__container} ${isEditable && style.task__nameEditable}` }>
         <CheckBox id={id} isDone={isDone} />
-        <span className={ style.task__name }>{text}</span>
+        <input 
+          className={ `${style.task__name} ${ isDone && style.task__Completed}` } 
+          type="text" 
+          value={isNameChanged(text, newTask)} 
+          contentEditable={isEditable} 
+          onChange={ event => setNewTask(event.target.value) } 
+        />
         <div className={ style.task__action }>
-          <HiOutlineDotsHorizontal className={ style.task__icon } onClick={() => setDialogActive(!dialogActive)}/>
-          <TaskAction open={dialogActive} id={id} />
+          {!isEditable && <HiOutlineDotsHorizontal className={ style.task__icon } onClick={() => setDialogActive(!dialogActive)}/>}
+          {isEditable && <LuCheck className={ style.task__icon } onClick={() => editNameTask(id, newTask)}/>}
+          <TaskAction open={dialogActive} id={id} name={newTask} setIsEditable={setIsEditable} setDialogActive={setDialogActive} />
         </div>
       </div>
     </li>
