40bb275208085f040e0418b113e9ed9efc5f4f0d
Perestra <danilo.ps.1996@gmail.com>
2024-09-18 21:45:18 -0300

    Removed the HeaderRouter and add each header component and 2 news routes

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { TaskProvider } from "./contexts/TaskContext"
-import { HeaderRouter } from "./components/HeaderRouter/HeaderRouter"
 import { AllTasks } from "./pages/AllTasks/AllTasks"
+import { ActiveTasks } from "./pages/ActiveTasks/ActiveTasks"
+import { CompletedTasks } from "./pages/CompletedTasks/CompletedTasks"
+import { Header } from "./components/Header/Header"
+import { Date } from "./components/Date/Date"
+import { Form } from "./components/Form/Form"
 
 const Routers = () => {
   return (
     <BrowserRouter>
-      <HeaderRouter/>
       <TaskProvider>
+        <Header />
+        <Date />
+        <Form />
         <Routes>
           <Route index element={<AllTasks />} />
+          <Route path="/completas" element={<CompletedTasks />} />
+          <Route path="/ativas" element={<ActiveTasks />} />
         </Routes>
       </TaskProvider>
     </BrowserRouter>
