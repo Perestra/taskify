50e850fc79e01aaba857b8008e160de870c9108e
Perestra <danilo.ps.1996@gmail.com>
2024-09-20 11:38:52 -0300

    Added the attribute title for accessibility

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,17 +3,19 @@ import React from 'react'
 type Props = {
   btnClass?: string;
   text?: string;
+  title?: string;
   icon?: React.ElementType;
   type: 'button' | 'submit' | 'reset';
   iconClass?: string;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const Button = ({ btnClass, text, icon: Icon, type, iconClass, onClick }: Props) => {
+export const Button = ({ btnClass, text, title, icon: Icon, type, iconClass, onClick }: Props) => {
   return (
     <button 
       className={ btnClass }
       aria-label={ text }
+      title={title}
       type={ type }
       value={ text }
       onClick={ onClick }
