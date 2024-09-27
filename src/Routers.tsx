d512262e8013b9309622047abcecf33c28f55cf6
Perestra <danilo.ps.1996@gmail.com>
2024-09-27 10:09:36 -0300

    Removed some default components in Router

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,24 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { TaskProvider } from "./contexts/TaskContext"
+import { Header } from "./components/Header/Header"
 import { AllTasks } from "./pages/AllTasks/AllTasks"
 import { ActiveTasks } from "./pages/ActiveTasks/ActiveTasks"
 import { CompletedTasks } from "./pages/CompletedTasks/CompletedTasks"
-import { Header } from "./components/Header/Header"
-import { Date } from "./components/Date/Date"
-import { Form } from "./components/Form/Form"
+import { NotFound } from "./pages/NotFound/NotFound"
 
 const Routers = () => {
   return (
     <BrowserRouter>
       <TaskProvider>
         <Header />
-        <Date />
-        <Form />
-        <Routes>
-          <Route index element={<AllTasks />} />
-          <Route path="/completas" element={<CompletedTasks />} />
-          <Route path="/ativas" element={<ActiveTasks />} />
-        </Routes>
+          <Routes>
+            <Route index element={<AllTasks />} />
+            <Route path="completas" element={<CompletedTasks />} />
+            <Route path="ativas" element={<ActiveTasks />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
       </TaskProvider>
     </BrowserRouter>
   )
