87bb8c739b9f15f9417e28eda95570ef9659589f
Perestra <danilo.ps.1996@gmail.com>
2024-09-27 10:08:53 -0300

    Created a new component to used in each page

---
+++ src/components/DateInputForm/DateInputForm .tsx
@@ -0,0 +1,13 @@
+import { Date } from '../Date/Date'
+import { Form } from '../Form/Form'
+
+const DateInputForm  = () => {
+  return (
+    <>
+      <Date />
+      <Form />
+    </>
+  )
+}
+
+export default DateInputForm 
